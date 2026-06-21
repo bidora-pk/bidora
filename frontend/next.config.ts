099ac5abe267b7cd5c0af826878f4fef9f7fb9be
bidora-pk <bidorapk@gmail.com>
2026-06-21 17:08:10 +0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // This tells Vercel to ignore TypeScript errors when building
   typescript: {
     ignoreBuildErrors: true,
-  },
-  // This tells Vercel to ignore ESLint formatting warnings when building
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
